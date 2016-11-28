6227ce20e1db1152f7236bd6845a231813ff744f
Lubomír Čuhel <cuhel.lubomir@gmail.com>
2016-11-28 22:53:10 +0100

    Vo všechno se postará bower

--- gulpfile.js
+++ gulpfile.js
@@ -1,26 +1,42 @@
 var gulp = require('gulp');
-var $gulp = require('gulp-load-plugins')();
+var plugins = require('gulp-load-plugins')();
+var mainBowerFiles = require('main-bower-files');
+var read = require('read-file');
+var bower = require('gulp-bower');
+
 var localServer = require('./server.js');
 
 gulp.task('js', function () {
     return gulp.src('./src.js/**/*.js')
-        .pipe($gulp.ngAnnotate())
-        .pipe($gulp.concat('app.js'))
+        .pipe(plugins.ngAnnotate())
+        .pipe(plugins.concat('app.js'))
         .pipe(gulp.dest('./build/client/'));
 });
 
 gulp.task('tmpl', function () {
     return gulp.src('./src.js/templates/*.html')
-        .pipe($gulp.angularTemplates({module: "MainModule"}))
+        .pipe(plugins.angularTemplates({module: "MainModule"}))
         .pipe(gulp.dest('./build/client/'));
 });
 
-gulp.task('build', ['js', 'tmpl'], function () {
-    return gulp.src('./build/client/*.js')
-        .pipe($gulp.concat('client.js'))
+gulp.task('libs', function () {
+    return bower()
+        .pipe(gulp.src(mainBowerFiles()))
+        .pipe(plugins.concat('libs.js'))
         .pipe(gulp.dest('./'))
-        .pipe($gulp.uglify())
-        .pipe($gulp.rename({
+        .pipe(plugins.uglify())
+        .pipe(plugins.rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('./'));
+});
+
+gulp.task('build', ['js', 'tmpl', 'libs'], function () {
+    return gulp.src('./build/client/*.js')
+        .pipe(plugins.concat('client.js'))
+        .pipe(gulp.dest('./'))
+        .pipe(plugins.uglify())
+        .pipe(plugins.rename({
             suffix: '.min'
         }))
         .pipe(gulp.dest('./'));
